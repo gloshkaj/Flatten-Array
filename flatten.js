68864c3c6e31f776994bba1927937524474e0c88
gloshkaj <garen.loshkajian@gmail.com>
2016-10-23 13:29:36 -0400

    Update flatten.js

--- flatten.js
+++ flatten.js
@@ -1,21 +1,18 @@
 var array = [[1, 2, [3]], 4];
 console.log(flatten(array));
-// This is done in a linear time O(n) without recursion
-// memory complexity is O(1) or O(n) if mutable param is set to false
 function flatten(array, mutable) {
     var toString = Object.prototype.toString;
     var arrayTypeStr = '[object Array]';
-
+    // Initialize an empty array
     var result = [];
     var nodes = (mutable && array) || array.slice();
     var node;
-
+    // If the array is empty then return the result
     if (!array.length) {
         return result;
     }
-
     node = nodes.pop();
-
+    // For each sub array in the larger array, remove the sub array and flatten it.
     do {
         if (toString.call(node) === arrayTypeStr) {
             nodes.push.apply(nodes, node);
@@ -23,7 +20,7 @@ function flatten(array, mutable) {
             result.push(node);
         }
     } while (nodes.length && (node = nodes.pop()) !== undefined);
-
-    result.reverse(); // we reverse result to restore the original order
+    // The result would be reversed so restore original order
+    result.reverse();
     return result;
 }
